06e7355708c4debdb7214ba66c920ef6313fb57f
you2336 <132917783+you2336@users.noreply.github.com>
2024-07-26 01:42:40 +0900

    タイトル画面にExpandedを追加

--- lib/main.dart
+++ lib/main.dart
@@ -88,28 +88,47 @@ class MyHomePage extends StatelessWidget {
       mainAxisAlignment: MainAxisAlignment.end,
       children: [
         Expanded(
-          flex: 4,
+          flex: 3,
           child: Container(
+            //color: Color.fromARGB(255, 255, 255, 255),
               //margin: const EdgeInsets.only(top: 20, left: 20, right: 20),
               child: Column(
             mainAxisAlignment: MainAxisAlignment.end,
             children: [
-              Text(
+              Expanded(
+                flex: 1,
+                child:
+              SizedBox()),
+              Expanded(
+                flex: 3,
+                child:
+              Container(
+                //color: Colors.black54,
+                child:
+                Text(
                 'の う も み',
                 style: TextStyle(fontSize: 150),
                 textAlign: TextAlign.center,
-              ),
+              ))),
+              Expanded(
+                flex: 2,
+                child:
+              Container(
+                //color: Colors.amberAccent,
+                child:
               Text(
                 '~ 脳力アップちゃれんじ ~\n',
                 style: TextStyle(fontSize: 50),
                 textAlign: TextAlign.center,
               ),
+              )
+              )
             ],
           )),
         ),
 
         //SizedBox(height: 50),
-        Expanded(flex: 4, child: sub_Column),
+        Expanded(flex: 2, child: sub_Column),
       ],
     );
 
@@ -119,8 +138,8 @@ class MyHomePage extends StatelessWidget {
       body: Center(child: LayoutBuilder(
           builder: (BuildContext context, BoxConstraints constrains) {
         return Container(
-            width: constrains.maxWidth,
-            height: constrains.maxHeight,
+            //width: constrains.maxWidth,
+            //height: constrains.maxHeight,
             child: main_Container);
       })),
     );
